d2bc35a749a168c57b4c045585ee21295c5c228c
kyubuns <kyubuns@gmail.com>
2021-02-20 11:48:29 +0900

    feat: メニューからGameObjectを作れる

---
+++ Assets/AbcConsole/Editor/CreateMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace AbcConsole.Editor
+{
+    public static class CreateMenu
+    {
+        [MenuItem("Assets/Create/AbcConsole")]
+        public static void Create()
+        {
+            var prefabGuid = AssetDatabase.FindAssets("AbcConsole t:Prefab").Single();
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(prefabGuid));
+
+            var instance = PrefabUtility.InstantiatePrefab(prefab);
+            instance.name = "AbcConsole";
+
+            Selection.objects = new[] { instance };
+        }
+    }
+}
