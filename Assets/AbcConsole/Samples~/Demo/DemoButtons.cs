85d9c319eb7edd12d28b20bf825418c49adf988c
kyubuns <kyubuns@gmail.com>
2021-02-20 15:59:49 +0900

    feat: Demo 押したらログが出るボタン

---
+++ Assets/AbcConsole/Samples~/Demo/DemoButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace AbcConsole.Demo
+{
+    public class DemoButtons : MonoBehaviour
+    {
+        public void OnClickPrintLogButton()
+        {
+            StartCoroutine(PrintLog());
+        }
+
+        private IEnumerator PrintLog()
+        {
+            for (var i = 0; i < 50; ++i)
+            {
+                Debug.Log($"PrintLog {i}");
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
