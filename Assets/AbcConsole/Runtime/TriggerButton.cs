c8ce947ffd094aee03fadeaa23a0d788540fdef9
kyubuns <kyubuns@gmail.com>
2021-02-20 12:32:12 +0900

    feat: 画面構成

---
+++ Assets/AbcConsole/Runtime/TriggerButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace AbcConsole
+{
+    public class TriggerButton : MonoBehaviour
+    {
+        [SerializeField] private GameObject console = default;
+
+        public void OnClick()
+        {
+            console.SetActive(!console.activeSelf);
+        }
+    }
+}
