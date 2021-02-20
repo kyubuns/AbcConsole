d9f296e9907eeef8b8716ea2d802e65dd8eff6a7
kyubuns <kyubuns@gmail.com>
2021-02-20 10:48:06 +0900

    feat: 基本ディレクトリ

---
+++ Assets/Editor/BeforeAssetStoreUpload.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Development
+{
+    public static class BeforeAssetStoreUpload
+    {
+        private const string Target = "Assets/AbcConsole";
+
+        [MenuItem("Asset Store Tools/**Before Upload**")]
+        public static void Run()
+        {
+            Debug.Log("Start");
+            AssetDatabase.DeleteAsset("Assets/AbcConsole/LICENSE.md");
+            AssetDatabase.DeleteAsset("Assets/AbcConsole/package.json");
+
+            var basePath = Path.Combine(Application.dataPath, "AbcConsole");
+            Directory.Move(Path.Combine(basePath, "Samples~/Demo"), Path.Combine(basePath, "Demo"));
+            File.Move(Path.Combine(basePath, "Samples~/Demo.meta"), Path.Combine(basePath, "Demo.meta"));
+
+            AssetDatabase.Refresh(ImportAssetOptions.Default);
+            Debug.Log("Finish");
+        }
+    }
+}
---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Development
+{
+    public static class PackageExporter
+    {
+        private const string Target = "Assets/AbcConsole";
+
+        [MenuItem("Export/Package")]
+        public static void Export()
+        {
+            var packageText = AssetDatabase.LoadAssetAtPath<TextAsset>(Path.Combine(Target, "package.json"));
+            var package = JsonUtility.FromJson<PackageJson>(packageText.text);
+
+            var basePath = Path.Combine(Application.dataPath, "AbcConsole");
+            Directory.Move(Path.Combine(basePath, "Samples~/Demo"), Path.Combine(basePath, "Demo"));
+            File.Move(Path.Combine(basePath, "Samples~/Demo.meta"), Path.Combine(basePath, "Demo.meta"));
+            AssetDatabase.Refresh(ImportAssetOptions.Default);
+
+            var outputPath = Path.Combine(Path.GetDirectoryName(Application.dataPath) ?? "", $"{package.displayName}_v{package.version}.unitypackage");
+            AssetDatabase.ExportPackage(new[] { Target }, outputPath, ExportPackageOptions.Recurse);
+            Debug.LogFormat("ExportPackage {0}", outputPath);
+        }
+    }
+
+    [Serializable]
+    public class PackageJson
+    {
+        public string displayName;
+        public string version;
+    }
+}
