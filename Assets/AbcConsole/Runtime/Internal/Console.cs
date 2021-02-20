6f6b955d637d9cc6006f920e1a558c75668443ad
kyubuns <kyubuns@gmail.com>
2021-02-20 17:00:26 +0900

    feat: デバッグコマンドが実行できる

--- Assets/AbcConsole/Runtime/Internal/Console.cs
+++ Assets/AbcConsole/Runtime/Internal/Console.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using AnKuchen.KuchenList;
 using AnKuchen.Map;
 using UnityEngine;
@@ -118,7 +122,51 @@ namespace AbcConsole.Internal
         {
             var text = _ui.InputField.text.Trim();
             _ui.InputField.text = "";
+            if (string.IsNullOrWhiteSpace(text)) return;
+
             Debug.Log($"> {text}");
+
+            var input = text.Split(' ').Select(x => x.Trim()).ToArray();
+
+            var method = _root.DebugCommands.FirstOrDefault(x => x.MethodInfo.Name == input[0]);
+            if (method == null)
+            {
+                Debug.Log($"{input[0]} is not found");
+                return;
+            }
+
+            var parameters = new List<object>();
+            var args = input.Skip(1).ToList();
+            var parameterInfos = method.MethodInfo.GetParameters();
+            if (parameterInfos.Length != args.Count)
+            {
+                Debug.Log($"{method.MethodInfo.Name} needs {parameterInfos.Length} parameters");
+                return;
+            }
+
+            foreach (var parameterInfo in parameterInfos)
+            {
+                var type = parameterInfo.ParameterType;
+                if (type.IsPrimitive)
+                {
+                    var value = args.Count > 0
+                        ? TypeDescriptor.GetConverter(type).ConvertFromString(args[0])
+                        : Activator.CreateInstance(type);
+                    parameters.Add(value);
+                }
+                else if (type == typeof(string))
+                {
+                    parameters.Add(args.Count > 0 ? args[0] : default);
+                }
+                else
+                {
+                    Debug.Log($"parse error: {type}");
+                }
+
+                if (args.Count > 0) args.RemoveAt(0);
+            }
+
+            method.MethodInfo.Invoke(null, parameters.ToArray());
         }
 
         public void OnClickPasteButton()
