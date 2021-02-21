66ee70c4fbd1baf58998d04172ee1971dc802fa3
kyubuns <kyubuns@gmail.com>
2021-02-21 10:52:06 +0900

    feat: update demo

--- Assets/AbcConsole/Samples~/Demo/DemoCommand.cs
+++ Assets/AbcConsole/Samples~/Demo/DemoCommand.cs
@@ -4,34 +4,43 @@ namespace AbcConsole.Demo
 {
     public static class DemoCommand
     {
-        [AbcCommand("アプリのバージョンを表示する")]
+        [AbcCommand("Show application version")]
         public static void AppVersion()
         {
             Debug.Log($"AppVersion is {Application.version}");
-        }
-
-        [AbcCommand("message to debug log")]
-        public static void Echo(string message)
-        {
-            Debug.Log(message);
-        }
-
-        [AbcCommand("warning")]
-        public static void EchoWarning(string message)
-        {
-            Debug.LogWarning(message);
-        }
-
-        [AbcCommand("error")]
-        public static void EchoError(string message)
-        {
-            Debug.LogError(message);
         }
 
         [AbcCommand]
         public static void SetBackgroundColor(int r, int g, int b)
         {
             GameObject.Find("Main Camera").GetComponent<Camera>().backgroundColor = new Color32((byte) r, (byte) g, (byte) b, 255);
+        }
+
+        [AbcCommand]
+        public static void SpawnCube()
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.name = "Cube";
+
+            Debug.Log("Spawn Cube Success!");
+        }
+
+        [AbcCommand]
+        public static void SetCubeColor(int r, int g, int b)
+        {
+            var cube = GameObject.Find("Cube");
+            cube.GetComponent<MeshRenderer>().material.color = new Color32((byte) r, (byte) g, (byte) b, 255);
+
+            Debug.Log($"Set Cube Color ({r}, {g}, {b})");
+        }
+
+        [AbcCommand]
+        public static void SetCubePosition(float x, float y, float z)
+        {
+            var cube = GameObject.Find("Cube");
+            cube.transform.position = new Vector3(x, y, z);
+
+            Debug.Log($"Set Cube Position ({x}, {y}, {z})");
         }
     }
 }
