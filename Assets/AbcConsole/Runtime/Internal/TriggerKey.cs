134cdb9f99280bd7d502733b73cccb90121bc10f
kyubuns <kyubuns@gmail.com>
2021-03-23 17:49:11 +0900

    feat: コンソールをEscapeキーでも抜けれる

--- Assets/AbcConsole/Runtime/Internal/TriggerKey.cs
+++ Assets/AbcConsole/Runtime/Internal/TriggerKey.cs
@@ -5,11 +5,24 @@ namespace AbcConsole.Internal
     public class TriggerKey : MonoBehaviour
     {
         [SerializeField] private KeyCode keyCode = KeyCode.BackQuote;
+        private Root _root;
+
+        public void Start()
+        {
+            _root = GetComponentInParent<Root>();
+        }
 
         public void Update()
         {
-            if (!Input.GetKeyDown(keyCode)) return;
-            GetComponentInParent<Root>().OnClickTriggerButton();
+            if (Input.GetKeyDown(keyCode))
+            {
+                _root.OnClickTriggerButton();
+            }
+
+            if (Root.CurrentInstance.State != ConsoleState.None && Input.GetKeyDown(KeyCode.Escape))
+            {
+                _root.OnClickTriggerButton();
+            }
         }
     }
 }
