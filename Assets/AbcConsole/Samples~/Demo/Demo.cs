95d80ffe60055d0491a8df2f74c2f60c992649c5
kyubuns <kyubuns@gmail.com>
2021-05-14 18:51:30 +0900

    feat: 最初から大量にログがあったときにスクロールするバグ再現

---
+++ Assets/AbcConsole/Samples~/Demo/Demo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace AbcConsole.Demo
+{
+    public class Demo : MonoBehaviour
+    {
+        public void Start()
+        {
+            for (var i = 0; i < 50; ++i)
+            {
+                Debug.Log($"Test {i}");
+            }
+        }
+    }
+
+}
