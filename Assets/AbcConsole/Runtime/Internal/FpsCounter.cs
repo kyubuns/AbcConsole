fc4032bc5ddf2ff680d8f70e0a52bc5e76fcdeb4
kyubuns <kyubuns@gmail.com>
2021-09-23 10:55:52 +0900

    feat: FPS Counter

--- Assets/AbcConsole/Runtime/Internal/FpsCounter.cs
+++ Assets/AbcConsole/Runtime/Internal/FpsCounter.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Profiling;
 
 namespace AbcConsole.Internal
 {
@@ -7,9 +10,88 @@ namespace AbcConsole.Internal
     {
         [SerializeField] private Text text = default;
 
+        private readonly List<FrameData> _frames = new List<FrameData>();
+
+        public void OnEnable()
+        {
+            _frames.Clear();
+            text.text = "";
+        }
+
         public void Update()
         {
-            text.text = $"{Time.time:0.00}";
+            var targetFrameRate = Application.targetFrameRate;
+            if (targetFrameRate <= 0) targetFrameRate = 30;
+
+            _frames.Add(Get());
+            while (_frames.Count > targetFrameRate)
+            {
+                _frames.RemoveAt(0);
+            }
+
+            if (Time.frameCount % targetFrameRate == 0)
+            {
+                var content = new StringBuilder();
+                var framesCount = _frames.Count;
+
+                {
+                    var total = 0f;
+                    for (var i = 0; i < framesCount; ++i) total += _frames[i].DeltaTime;
+                    content.Append("FPS: ");
+                    content.AppendFormat("{0:0.00}", 1f / (total / framesCount));
+                }
+
+                content.Append("\n");
+
+                content.Append("Memory:");
+                content.Append("\n");
+
+                {
+                    var total = 0.0;
+                    for (var i = 0; i < framesCount; ++i) total += _frames[i].TotalAllocatedMemory;
+                    content.Append("  Allocated: ");
+                    content.AppendFormat("{0:0}MB", total / 1024 / 1024);
+                }
+
+                content.Append("\n");
+
+                {
+                    var total = 0.0;
+                    for (var i = 0; i < framesCount; ++i) total += _frames[i].TotalReservedMemory;
+                    content.Append("  Reserved: ");
+                    content.AppendFormat("{0:0}MB", total / 1024 / 1024);
+                }
+
+                content.Append("\n");
+
+                {
+                    var total = 0.0;
+                    for (var i = 0; i < framesCount; ++i) total += _frames[i].AllocatedMemoryForGraphicsDriver;
+                    content.Append("  Allocated(GPU): ");
+                    content.AppendFormat("{0:0}MB", total / 1024 / 1024);
+                }
+
+                text.text = content.ToString();
+            }
         }
+
+        private FrameData Get()
+        {
+            return new FrameData
+            {
+                DeltaTime = Time.unscaledDeltaTime,
+                AllocatedMemoryForGraphicsDriver = Profiler.GetAllocatedMemoryForGraphicsDriver(),
+                TotalAllocatedMemory = Profiler.GetTotalAllocatedMemoryLong(),
+                TotalReservedMemory = Profiler.GetTotalReservedMemoryLong(),
+            };
+        }
+    }
+
+    public struct FrameData
+    {
+        public float DeltaTime { get; set; }
+        public long AllocatedMemoryForGraphicsDriver { get; set; }
+        public long TotalAllocatedMemory { get; set; }
+        public long TotalReservedMemory { get; set; }
     }
 }
