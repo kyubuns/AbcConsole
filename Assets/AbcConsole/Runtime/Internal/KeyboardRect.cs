e49f3521a972e2f0882abb09fa5cd210c230d788
kyubuns <kyubuns@gmail.com>
2025-08-04 20:36:17 +0900

    fix: Unity6

--- Assets/AbcConsole/Runtime/Internal/KeyboardRect.cs
+++ Assets/AbcConsole/Runtime/Internal/KeyboardRect.cs
@@ -13,35 +13,7 @@ namespace AbcConsole.Internal
 
             if (Application.platform == RuntimePlatform.Android)
             {
-                using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-                {
-                    var currentActivity = unityClass.GetStatic<AndroidJavaObject>("currentActivity");
-                    var unityPlayer = currentActivity.Get<AndroidJavaObject>("mUnityPlayer");
-                    var view = unityPlayer.Call<AndroidJavaObject>("getView");
-
-                    if (view == null) return 0;
-
-                    int result;
-
-                    using (var rect = new AndroidJavaObject("android.graphics.Rect"))
-                    {
-                        view.Call("getWindowVisibleDisplayFrame", rect);
-                        result = Screen.height - rect.Call<int>("height");
-                    }
-
-                    if (TouchScreenKeyboard.hideInput) return result;
-
-                    var softInputDialog = unityPlayer.Get<AndroidJavaObject>("mSoftInputDialog");
-                    var window = softInputDialog?.Call<AndroidJavaObject>("getWindow");
-                    var decorView = window?.Call<AndroidJavaObject>("getDecorView");
-
-                    if (decorView == null) return result;
-
-                    var decorHeight = decorView.Call<int>("getHeight");
-                    result += decorHeight;
-
-                    return result;
-                }
+                return 0f;
             }
 
             if (Application.platform == RuntimePlatform.IPhonePlayer)
